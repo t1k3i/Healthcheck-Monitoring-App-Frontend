220a007381c751228f26617af60ac1f4fb00ad59
Tikei <radovac.tikei@gmail.com>
2024-07-17 15:16:33 +0200

    Implemented username and password validation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,15 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { authInterceptor } from './interceptors/auth.interceptor';
+import { FormsModule } from '@angular/forms';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withFetch(),withInterceptors([authInterceptor]))
+    provideHttpClient(withFetch(),withInterceptors([authInterceptor])),
+    importProvidersFrom(FormsModule)
   ]
 };
--- src/app/modals/login/login.component.ts
+++ src/app/modals/login/login.component.ts
@@ -1,42 +1,54 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { AuthenticationService } from '../../services/authentication.service';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
 
-  username = '';
-  password = '';
   @ViewChild('closebutton') closebutton!: ElementRef;
+  @ViewChild('userForm') userForm!: NgForm; // Reference to NgForm
+
+  public authError: string = '';
 
   constructor(private authService: AuthenticationService) {}
 
-  public onLogin(): void {
-    this.authService.login(this.username, this.password).subscribe(
+  public onLogin(form: NgForm): void {
+    this.authService.login(form.value.username, form.value.password).subscribe(
       success => {
-        this.close();
-        console.log('Login successful ');
+        form.resetForm();
+        this.close(form);
       },
       error => {
-        console.error('Login failed', error);
+        this.authError = 'User not found';
       }
     );
   }
 
-  private clear(): void {
-    this.username = '';
-    this.password = '';
+  public close(form: NgForm): void {
+    form.resetForm();
+    this.closebutton.nativeElement.click();
+    this.authError = '';
   }
 
-  public close(): void {
-    this.clear();
-    this.closebutton.nativeElement.click();
+  // Host listener for clicking outside the modal or pressing escape key
+  @HostListener('document:click', ['$event'])
+  @HostListener('document:keyup.escape', ['$event'])
+  onDocumentEvent(event: MouseEvent | KeyboardEvent): void {
+    const target = event.target as HTMLElement;
+    const modalElement = document.getElementById('login-form');
+    if (modalElement && !modalElement.contains(target)) {
+      this.userForm.resetForm();
+      this.authError = '';
+    } else if (event instanceof KeyboardEvent && event.key === 'Escape') {
+      this.userForm.resetForm();
+      this.authError = '';
+    }
   }
 
 }
