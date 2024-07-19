132fedc62fa6abeb0d4bd1b84d134b15613f8e82
Tikei <radovac.tikei@gmail.com>
2024-07-19 10:33:55 +0200

    Small change

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,10 +1,10 @@
 import { Role } from "./role";
 
 export interface User {
+    id: number;
     firstName: string;
     lastName: string;
     username: string;
-    password: string;
     role: Role;
     authdata?: string;
 }
